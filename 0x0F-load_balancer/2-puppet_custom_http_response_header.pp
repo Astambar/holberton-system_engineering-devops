12e089ba38520490822d062b9bfdc2f08fc42d01
Astambar <leuchartjason@gmail.com>
2022-08-23 03:34:01 +0000

    project 0x0F

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+# Ajouter un header HTTP personnalisé avec Puppet
+exec {'apt update -y':
+  command => 'apt update -y'
+  path    => ['/usr/bin'],
+}
+
+exec {'apt install -y nginx':
+  command => 'apt install -y nginx'
+  path    => ['/usr/bin'],
+}
+
+file_line { '/etc/nginx/sites-available/default':
+  path => '/etc/nginx/sites-available/default',
+  line => '    add_header X-Served-By $(hostname);',
+}
+
+exec {'service nginx restart':
+  command => 'service nginx restart'
+  path    => ['/usr/bin'],
+}
