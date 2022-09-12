65bdca5edf9107d866063914fbf2242918805739
Astambar <leuchartjason@gmail.com>
2022-09-12 19:11:06 +0000

    0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -29,7 +29,8 @@ if __name__ == "__main__":
             did += 1
         total += 1
 
-    print("Employee {} is done with tasks({}/{}):".format(userName, did, total))
+    print("Employee {} is done with tasks({}/{}):".format(userName, did, total)
+          )
 
     for idx in todoList:
         print("\t " + idx)
