5a6dcd257d3c09c533702733f402a506f6b51ee1
Astambar <leuchartjason@gmail.com>
2022-09-27 18:34:04 +0000

    mandatory task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,10 +7,8 @@ import requests
 
 def top_ten(subreddit):
     """
-    The top_ten function takes a subreddit as
-    an argument and returns the top 10
-    hot posts for that subreddit.
-    If the subreddit does not exist, it returns None.
+    The top_ten function takes a subreddit as an argument and returns the top 10
+    hot posts for that subreddit. If the subreddit does not exist, it returns None.
 
     :param subreddit: Pass the name of the subreddit to be analyzed
     :return: The titles of the first 10 hot posts listed for a given subreddit
@@ -21,11 +19,9 @@ def top_ten(subreddit):
         "https://www.reddit.com/r/" + subreddit + "/hot.json",
         headers={'User-Agent': 'Holberton'}
     )
-    tmpPrinter = ""
     if responseIsReq.status_code == 404:
-        tmpPrinter += "None\n"
+         print("None")
     else:
         data = responseIsReq.json()["data"]["children"]
         for title in data[:10]:
-            tmpPrinter += title["data"]["title"]+"\n"
-    print(tmpPrinter)
+            print(title["data"]["title"])
