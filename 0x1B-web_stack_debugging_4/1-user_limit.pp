45c0b697b0769b54ed79baccb446c5803032eed9
Astambar <leuchartjason@gmail.com>
2022-10-06 21:14:05 +0000

    advanced task

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,2 @@
-/*
-Modifie la configuration du système d'exploitation afin qu'il soit possible de se connecter
-avec l'utilisateur holberton et ouvre un fichier sans aucun message d'erreur
-*/
+# Modifie la configuration du système d'exploitation afin qu'il soit possible de se connecter avec l'utilisateur holberton et ouvre un fichier sans aucun message d'erreur
 exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
