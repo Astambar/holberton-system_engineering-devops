cedbb2d7b6caaba4106238f205db957667572bf8
Astambar <leuchartjason@gmail.com>
2022-10-06 21:28:32 +0000

    advanced task

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # augmenter la correction des limites matérielles et logicielles Trop d'erreurs de fichiers ouverts
 
-exec { 'soft-limit':
+exec { 'limit-soft':
   command => 'echo "nginx       soft    holberton   1000" > /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-exec { 'hard-limit':
+exec { 'limit-hard':
   command => 'echo "nginx       hard    holberton  2000" > /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
