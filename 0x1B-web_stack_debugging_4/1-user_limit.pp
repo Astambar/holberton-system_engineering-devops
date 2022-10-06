8a4a0b2ff51d8b049a7e6c35854e843d77bd3624
Astambar <leuchartjason@gmail.com>
2022-10-06 21:20:02 +0000

    advanced task

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,2 +1,11 @@
 # Modifie la configuration du système d'exploitation afin qu'il soit possible de se connecter avec l'utilisateur holberton et ouvre un fichier sans aucun message d'erreur
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
+
+exec { 'soft-limit':
+  command => 'echo "nginx       soft    holberton   1000" > /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'hard-limit':
+  command => 'echo "nginx       hard    holberton  2000" > /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
